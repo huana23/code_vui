3220fd03104e3654a603f65c2f4434b9b853ea99
An <anaquacu@gmail.com>
2022-02-23 22:54:55 +0700

    first commit

---
+++ assest/js/count.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+    titleHeader: "An có đẹp trai không",
+    descriptionHeader:
+      "Có hai lựa chọn cho em : ^^",
+    buttonYes: "Đẹp",
+    buttonNo: "Tất nhiên là xấu rồi",
+    titleModar: "Anh biết em mà không đẹp làm sao mà được",
+    descriptionModar:
+      'Đẹp trai số 2 thì không có ai đẹp trai số 1 hị . ',
+  };
